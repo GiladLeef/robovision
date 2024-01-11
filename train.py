1107b58a1d500477e63d2d9724dbc84f321245f6
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2024-01-11 13:05:59 +0200

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ def main():
     # Load a model
     model = YOLO('dataset\\model.yaml')  # build a new model from YAML
     # Train the model
-    results = model.train(data='dataset\\data.yaml', epochs=100, imgsz=640)
+    results = model.train(data='dataset\\dataset.yaml', epochs=100, imgsz=640)
 
 if __name__ == "__main__":
     main()
