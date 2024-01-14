b5b1ccab3f1413e2e6576aec0f50f12d3dc2eab7
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2024-01-14 07:30:13 +0200

    Update export.py

--- robovision/models/export.py
+++ robovision/models/export.py
@@ -4,5 +4,4 @@ from ultralytics import YOLO
 model = YOLO('best.pt')  # load a custom trained model
 
 # Export the model
-model.export(format='onnx')
-#model.export(format='onnx', imgsz=360)
+model.export(format='onnx', imgsz=320)
