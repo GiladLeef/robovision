72e38d7d18172ffd399f92666eb1fad6ef7db938
GiladLeef <96906027+GiladLeef@users.noreply.github.com>
2024-12-12 09:27:09 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 def main():
     # Load pretrained model weights
-    model = YOLO('robovision\\models\\best.pt')
+    # model = YOLO('robovision\\models\\best.pt')
     
     # Pretrain from scratch. load configuration from the model.yaml file:
-    # model = YOLO('dataset\\model.yaml')  
+    model = YOLO('dataset\\model.yaml')  
 
     # Train the model
     results = model.train(data='dataset\\dataset.yaml', epochs=100, imgsz=640)
